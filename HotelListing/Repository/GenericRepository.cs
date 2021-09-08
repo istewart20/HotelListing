e1f4d71fbf41ad23bd35c4f471f9f5d29d9f510a
Iain Stewart <iain.stewart08@gmail.com>
2021-09-08 23:15:09 +0100

    Setup generic repository and start to implement

---
+++ HotelListing/Repository/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using HotelListing.Data;
+using HotelListing.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DatabaseContext _context;
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(DatabaseContext context)
+        {
+            _context = context;
+            _db = _context.Set<T>();
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await _db.FindAsync(id);
+            _db.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _db.RemoveRange(entities);
+        }
+
+        public Task<T> Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _db.AddAsync(entity);
+        }
+
+        public async Task InsertRange(IEnumerable<T> entities)
+        {
+            await _db.AddRangeAsync(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _db.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
